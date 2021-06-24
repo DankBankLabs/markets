3088708d16d88eab8faf431cf0e0087ebd21f819
0xcroc <0xcroc@gmail.com>
2021-06-23 21:24:18 -0400

    chore: 2 space tabs to 4 space

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,8 @@ import "hardhat-gas-reporter";
 import "solidity-coverage";
 import "@openzeppelin/hardhat-defender";
 import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-ethers";
+import "hardhat-deploy";
 
 import "./tasks/accounts";
 import "./tasks/clean";
@@ -17,24 +19,24 @@ import { NetworkUserConfig } from "hardhat/types";
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
 const chainIds = {
-  ganache: 1337,
-  goerli: 5,
-  hardhat: 31337,
-  kovan: 42,
-  mainnet: 1,
-  rinkeby: 4,
-  ropsten: 3,
+    ganache: 1337,
+    goerli: 5,
+    hardhat: 31337,
+    kovan: 42,
+    mainnet: 1,
+    rinkeby: 4,
+    ropsten: 3,
 };
 
 // Ensure that we have all the environment variables we need.
 const mnemonic = process.env.MNEMONIC;
 if (!mnemonic) {
-  throw new Error("Please set your MNEMONIC in a .env file");
+    throw new Error("Please set your MNEMONIC in a .env file");
 }
 
 const infuraApiKey = process.env.INFURA_API_KEY;
 if (!infuraApiKey) {
-  throw new Error("Please set your INFURA_API_KEY in a .env file");
+    throw new Error("Please set your INFURA_API_KEY in a .env file");
 }
 
 const defenderApiKey = process.env.DEFENDER_API_KEY;
@@ -42,81 +44,85 @@ const defenderSecret = process.env.DEFENDER_SECRET;
 
 let defenderSettings = {};
 if (defenderApiKey && defenderSecret) {
-  defenderSettings = {
-    apiKey: defenderApiKey,
-    apiSecret: defenderSecret,
-  };
+    defenderSettings = {
+        apiKey: defenderApiKey,
+        apiSecret: defenderSecret,
+    };
 }
 
 let etherscanKey = process.env.ETHERSCAN_API_KEY;
 const etherscanSettings = { apiKey: etherscanKey };
 
 function createTestnetConfig(network: keyof typeof chainIds): NetworkUserConfig {
-  const url: string = "https://" + network + ".infura.io/v3/" + infuraApiKey;
-  return {
-    accounts: {
-      count: 10,
-      initialIndex: 0,
-      mnemonic,
-      path: "m/44'/60'/0'/0",
-    },
-    chainId: chainIds[network],
-    url,
-  };
+    const url: string = "https://" + network + ".infura.io/v3/" + infuraApiKey;
+    return {
+        accounts: {
+            count: 10,
+            initialIndex: 0,
+            mnemonic,
+            path: "m/44'/60'/0'/0",
+        },
+        chainId: chainIds[network],
+        url,
+    };
 }
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "hardhat",
-  gasReporter: {
-    currency: "USD",
-    enabled: process.env.REPORT_GAS ? true : false,
-    excludeContracts: [],
-    src: "./contracts",
-  },
-  networks: {
-    hardhat: {
-      accounts: {
-        mnemonic,
-      },
-      chainId: chainIds.hardhat,
+    defaultNetwork: "hardhat",
+    namedAccounts: {
+        deployer: 0, // Do not use this account for testing
+        admin: 1,
     },
-    goerli: createTestnetConfig("goerli"),
-    kovan: createTestnetConfig("kovan"),
-    rinkeby: createTestnetConfig("rinkeby"),
-    ropsten: createTestnetConfig("ropsten"),
-  },
-  paths: {
-    artifacts: "./artifacts",
-    cache: "./cache",
-    sources: "./contracts",
-    tests: "./test",
-  },
-  solidity: {
-    version: "0.8.5",
-    settings: {
-      metadata: {
-        // Not including the metadata hash
-        // https://github.com/paulrberg/solidity-template/issues/31
-        bytecodeHash: "none",
-      },
-      // You should disable the optimizer when debugging
-      // https://hardhat.org/hardhat-network/#solidity-optimizer-support
-      optimizer: {
-        enabled: true,
-        runs: 800,
-      },
+    gasReporter: {
+        currency: "USD",
+        enabled: process.env.REPORT_GAS ? true : false,
+        excludeContracts: [],
+        src: "./contracts",
     },
-  },
-  typechain: {
-    outDir: "typechain",
-    target: "ethers-v5",
-  },
-  defender: {
-    ...defenderSettings,
-  },
-  etherscan: {
-    ...etherscanSettings,
-  },
+    networks: {
+        hardhat: {
+            accounts: {
+                mnemonic,
+            },
+            chainId: chainIds.hardhat,
+        },
+        goerli: createTestnetConfig("goerli"),
+        kovan: createTestnetConfig("kovan"),
+        rinkeby: createTestnetConfig("rinkeby"),
+        ropsten: createTestnetConfig("ropsten"),
+    },
+    paths: {
+        artifacts: "./artifacts",
+        cache: "./cache",
+        sources: "./contracts",
+        tests: "./test",
+    },
+    solidity: {
+        version: "0.8.5",
+        settings: {
+            metadata: {
+                // Not including the metadata hash
+                // https://github.com/paulrberg/solidity-template/issues/31
+                bytecodeHash: "none",
+            },
+            // You should disable the optimizer when debugging
+            // https://hardhat.org/hardhat-network/#solidity-optimizer-support
+            optimizer: {
+                enabled: true,
+                runs: 800,
+            },
+        },
+    },
+    typechain: {
+        outDir: "typechain",
+        target: "ethers-v5",
+    },
+    defender: {
+        ...defenderSettings,
+    },
+    etherscan: {
+        ...etherscanSettings,
+    },
 };
 
 export default config;
--- test/Greeter.ts
+++ test/Greeter.ts
@@ -9,20 +9,20 @@ import { shouldBehaveLikeGreeter } from "./Greeter.behavior";
 const { deployContract } = hre.waffle;
 
 describe("Unit tests", function () {
-  before(async function () {
-    this.signers = {} as Signers;
+    before(async function () {
+        this.signers = {} as Signers;
 
-    const signers: SignerWithAddress[] = await hre.ethers.getSigners();
-    this.signers.admin = signers[0];
-  });
-
-  describe("Greeter", function () {
-    beforeEach(async function () {
-      const greeting: string = "Hello, world!";
-      const greeterArtifact: Artifact = await hre.artifacts.readArtifact("Greeter");
-      this.greeter = <Greeter>await deployContract(this.signers.admin, greeterArtifact, [greeting]);
+        const signers: SignerWithAddress[] = await hre.ethers.getSigners();
+        this.signers.admin = signers[0];
     });
 
-    shouldBehaveLikeGreeter();
-  });
+    describe("Greeter", function () {
+        beforeEach(async function () {
+            const greeting: string = "Hello, world!";
+            const greeterArtifact: Artifact = await hre.artifacts.readArtifact("Greeter");
+            this.greeter = <Greeter>await deployContract(this.signers.admin, greeterArtifact, [greeting]);
+        });
+
+        shouldBehaveLikeGreeter();
+    });
 });
