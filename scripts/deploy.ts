3dceb8780b58d8463feeeb4e679a28892c0d725d
0xcroc <0xcroc@gmail.com>
2021-07-09 01:23:56 -0400

    feat: deploy on goerli

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,11 +1,11 @@
 import { ethers, upgrades } from "hardhat";
 
 async function main() {
-    const greeterFactory = await ethers.getContractFactory("Greeter");
+    const marketFactory = await ethers.getContractFactory("DankBankMarket");
 
-    const greeter = await upgrades.deployProxy(greeterFactory, ["Wassup"], { initializer: "init" });
+    const market = await upgrades.deployProxy(marketFactory, ["DankBankMarket does not give a fuck about a uri"], { initializer: "init" });
 
-    console.log({ greeter });
+    console.log({ market });
 }
 
 main()
