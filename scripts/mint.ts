7d36b1f79aa315ca734529bc5511b9f0d3daa7c9
christian-miljkovic <christian.miljkovic@gmail.com>
2021-07-23 12:34:51 -0400

    feat: mint on goerli

--- scripts/mint.ts
+++ scripts/mint.ts
@@ -2,8 +2,6 @@ import { BigNumber, Contract } from "ethers";
 import { ethers } from "hardhat";
 import NFT_VAULT from "../abi/ERC721VaultFactory.json";
 import { NFT_VAULT_CONTRACT_ADDRESS } from "../src/constants";
-import { ERC721Mock } from "../typechain";
-import { deploy } from "../test/helpers";
 
 const NFT_NAME = "Nft Test";
 const NFT_SYMBOL = "TNT";
@@ -14,19 +12,23 @@ const FEE = "10";
 async function main() {
     const [signer] = await ethers.getSigners();
 
-    const erc721Mock = await deploy<ERC721Mock>("ERC721Mock", { args: ["Mock NFT", "MNT"], connect: signer });
-    const fractional = new Contract(NFT_VAULT_CONTRACT_ADDRESS, NFT_VAULT, signer);
-
-    const tokenId = BigNumber.from(Math.floor(Math.random() * 100));
-    await erc721Mock.mint(signer.address, tokenId);
+    const Erc721Mock = await ethers.getContractFactory("ERC721Mock");
+    const erc721Mock = await Erc721Mock.deploy(NFT_NAME, NFT_SYMBOL);
 
     const tokenAddress = erc721Mock.address;
+    console.debug("Erc721 deployed to:", tokenAddress);
+    const fractional = new Contract(NFT_VAULT_CONTRACT_ADDRESS, NFT_VAULT, signer);
 
-    await erc721Mock.setApprovalForAll(fractional.address, true);
+    const tokenId = BigNumber.from(Math.floor(Math.random() * 10));
+
+    const mintTx = await erc721Mock.mint(signer.address, tokenId);
+    await mintTx.wait();
+    const approvalTx = await erc721Mock.setApprovalForAll(fractional.address, true);
+    await approvalTx.wait();
+
     const response = await fractional.mint(NFT_NAME, NFT_SYMBOL, tokenAddress, tokenId, SUPPLY, LIST_PRICE, FEE);
-
-    // const receipt = await response.wait();
-    // console.debug(receipt);
+    const receipt = await response.wait();
+    console.debug({ receipt });
 }
 
 main()
