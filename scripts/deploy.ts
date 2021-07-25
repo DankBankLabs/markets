fc6028d2774576a6474458f1d4079d6e55c5b67b
christian-miljkovic <christian.miljkovic@gmail.com>
2021-07-25 07:38:24 +0100

    wip: buy/sell tokens

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,7 +3,9 @@ import { ethers, upgrades } from "hardhat";
 async function main() {
     const marketFactory = await ethers.getContractFactory("DankBankMarket");
 
-    const market = await upgrades.deployProxy(marketFactory, ["DankBankMarket does not give a fuck about a uri"], { initializer: "init" });
+    const market = await upgrades.deployProxy(marketFactory, ["DankBankMarket does not give a fuck about a uri"], {
+        initializer: "init",
+    });
 
     console.log({ market });
 }
