ce71021e44eae1c9f676efd89e811c7818f0d297
christian-miljkovic <christian.miljkovic@gmail.com>
2021-09-01 00:14:00 +0200

    fix: hre compile error + oz import

--- contracts/DankBankMarket.sol
+++ contracts/DankBankMarket.sol
@@ -6,7 +6,7 @@ import "./ERC1155LPTokenUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract DankBankMarket is DankBankMarketData, Initializable, ERC1155LPTokenUpgradeable, ReentrancyGuard {
     using SafeERC20 for IERC20;
