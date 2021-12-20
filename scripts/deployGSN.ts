d04f7e0a0a2a13b1de2ee7d0b3b8684fbf370b55
Christian Miljkovic <christian.miljkovic@gmail.com>
2021-12-20 17:47:16 -0500

    GSN integration (#20)
    * feat: adding gsn to new DB contract
    
    * fix: compilation errors
    
    * fix: gsn deploy script
    
    * fix: address bug for upgradable contract

---
+++ scripts/deployGSN.ts
@@ -0,0 +1,27 @@
+import { ethers, upgrades } from "hardhat";
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const ForwarderFactory = await ethers.getContractFactory("MinimalForwarder");
+    const MarketGSNFactory = await ethers.getContractFactory("DankBankMarketGSN");
+
+    const forwarder = await ForwarderFactory.deploy();
+    console.log(`Forwarder Address: ${forwarder.address}`);
+
+    const gsnMarket = await upgrades.deployProxy(MarketGSNFactory, ["un-used uri", forwarder.address], {
+        initializer: "init",
+    });
+
+    console.log(`GSN Market Address: ${gsnMarket.address}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
