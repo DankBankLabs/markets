b441d95bf9ca32923024e3a9e737780b7c9f1928
Jack Gerulskis <jfgerulskis@wpi.edu>
2022-02-25 15:20:02 -0500

    test token deployment

---
+++ scripts/deploy-erc20.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+
+async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    console.log("Account balance:", (await deployer.getBalance()).toString());
+
+    const tokenFactory = await ethers.getContractFactory("MockToken");
+
+    const token = await tokenFactory.deploy();
+
+    console.log(`The mock token address is ${token.address} with total supply`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
