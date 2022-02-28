c5d7939b3c289015a4ad414ec128c9bc2dfafdad
christian-miljkovic <christian.miljkovic@gmail.com>
2022-02-28 11:50:52 -0500

    feat: deployed GSN to matic

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MockToken is ERC20 {
     uint256 constant _initial_supply = 1000000 * (10**18);
 
-    constructor() ERC20("Meme Test", "MTM") {
+    constructor() ERC20("Dank Test", "DTD") {
         _mint(msg.sender, _initial_supply);
     }
 }
